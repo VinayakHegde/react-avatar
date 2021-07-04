0ddac9456a63b4fca134bf0be7df135c89e13d69
Vinayak hegde <vhegde@wundermancommerce.com>
2021-07-04 21:19:47 +0100

    react avatar

---
+++ example/app.jsx
@@ -0,0 +1,124 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Avatar from '../src/avatar.jsx'
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    const src = SOURCE_PATH + '/einshtein.jpg'
+    this.state = {
+      preview: null,
+      defaultPreview: null,
+      src
+    }
+    this.onCrop = this.onCrop.bind(this)
+    this.onCropDefault = this.onCropDefault.bind(this)
+    this.onClose = this.onClose.bind(this)
+    this.onCloseDefault = this.onCloseDefault.bind(this)
+  }
+  dataURLtoFile(dataurl) {
+
+    var arr = dataurl.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n);
+
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    const ext = mime.slice('/')[1];
+    return new File([u8arr], `avatar.${ext}`, {type:mime});
+}
+  onCropDefault(preview) {
+    console.log(this.dataURLtoFile(preview, 'abc.png'))
+    this.setState({defaultPreview: preview})
+  }
+
+  onCrop(preview) {
+    console.log(preview)
+    this.setState({preview})
+  }
+
+  onCloseDefault() {
+    this.setState({defaultPreview: null})
+  }
+
+  onClose() {
+    this.setState({preview: null})
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row" style={{backgroundColor: '#a75d61', padding: '8px 0'}}>
+          <div className="col-2"/>
+          <div className="col-8">
+            <img src={SOURCE_PATH + '/user.png'} alt="" style={{ marginTop: '3px', marginRight: '15px', float: 'left', width: '36px', height: '36px'}}/>
+            <h1 style={{ marginTop: '3px', color: 'white', fontWeight: 300, fontSize: '2rem'}}>React avatar editor</h1>
+          </div>
+          <div className="col-2"/>
+        </div>
+        <div className="row" style={{marginTop: '45px'}}>
+          <div className="col-2"/>
+          <div className="col-8">
+            <h4>Default usage</h4>
+          </div>
+          <div className="col-2"/>
+        </div>
+        <div className="row">
+          <div className="col-2"/>
+          <div className="col-5">
+            <Avatar
+              width={390}
+              height={295}
+              onCrop={this.onCropDefault}
+              onClose={this.onCloseDefault}
+              // src={this.state.src}
+            />
+          </div>
+          <div className="col-2">
+            <h5>Preview</h5>
+            <img alt="" style={{width: '150px', height: '150px'}} src={this.state.defaultPreview}/>
+          </div>
+          <div className="col-3"/>
+        </div>
+        {/* <div className="row" style={{marginTop: '45px'}}>
+          <div className="col-2"/>
+          <div className="col-8">
+            <h4>With provided <code>src</code> property</h4>
+          </div>
+          <div className="col-2"/>
+        </div>
+        <div className="row">
+          <div className="col-2"/>
+          <div className="col-5">
+            <Avatar
+              width={390}
+              height={295}
+              cropRadius={50}
+              onCrop={this.onCrop}
+              onClose={this.onClose}
+              src={this.state.src}
+            />
+          </div>
+          <div className="col-2">
+            <h5>Preview</h5>
+            <img alt="" style={{width: '150px', height: '150px'}} src={this.state.preview}/>
+          </div>
+          <div className="col-3"/>
+        </div>
+        <div className="row" style={{backgroundColor: '#b3aeae', marginTop: '45px'}}>
+          <div className="col-2"/>
+          <div className="col-8" style={{margin: '25px 0'}}>
+            <a style={{color: '#ffffff'}} href="https://github.com/kirill3333/react-avatar">Fork me on Github</a>
+          </div>
+          <div className="col-2"/>
+        </div>*/}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App /> , document.getElementById('root'))
